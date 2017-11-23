54cad1326cf89ea5991ff40d209d5c3f503f620f
DUGPLPW7 <DUGPLPW7@10.30.15.57>
2017-11-22 21:33:15 -0600

    Initial commit

---
+++ build/classes-generated/org/unitec/basedatos/Bundle.java
@@ -0,0 +1,27 @@
+package org.unitec.basedatos;
+/** Localizable strings for {@link org.unitec.basedatos}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Principal</i>
+     * @see PrincipalTopComponent
+     */
+    static String CTL_PrincipalAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PrincipalAction");
+    }
+    /**
+     * @return <i>Principal Window</i>
+     * @see PrincipalTopComponent
+     */
+    static String CTL_PrincipalTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_PrincipalTopComponent");
+    }
+    /**
+     * @return <i>This is a Principal window</i>
+     * @see PrincipalTopComponent
+     */
+    static String HINT_PrincipalTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_PrincipalTopComponent");
+    }
+    private Bundle() {}
+}
